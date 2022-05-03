0fd53f5594c89da509b1900624b0d7f020ab30bc
August Manniche Møller <august.manniche@gmail.com>
2022-05-03 10:03:00 +0200

    Jumping and top corner done

--- Assets/Scripts/FireDamage.cs
+++ Assets/Scripts/FireDamage.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class FireDamage : MonoBehaviour
 {
 
-    public float damage= 10f;
+    public float damage= 5f;
+    public bool isInFire = false;
+
+    private IEnumerator coroutine;
 
 
     void OnTriggerEnter(Collider collision)
     {
+        
         if(collision.gameObject.tag=="Player")
         {
             PlayerHealth playerTarget = collision.transform.gameObject.GetComponent<PlayerHealth>();
             playerTarget.ApplyDamage(damage);
+            isInFire = true;
             Debug.Log("Player enter fire");
         }
         else if(collision.gameObject.tag=="Enemy")
@@ -21,36 +26,40 @@ public class FireDamage : MonoBehaviour
             EnemyTarget target = collision.transform.gameObject.GetComponent<EnemyTarget>();
             target.ApplyDamage(damage);
         }
+        
     }
+
 
     void OnTriggerStay(Collider collision)
     {
-        if(collision.gameObject.tag=="Player")
-        {
-            PlayerHealth playerTarget = collision.transform.gameObject.GetComponent<PlayerHealth>();
-            damage += 2f;
-            playerTarget.ApplyDamage(damage);
-            
-            Debug.Log("Player Took damage = " + damage);
-        }
-        else if(collision.gameObject.tag=="Enemy")
-        {
-            EnemyTarget target = collision.transform.gameObject.GetComponent<EnemyTarget>();
-            damage += 2f;
-            target.ApplyDamage(damage);
-        }
+            if(collision.gameObject.tag=="Player" && isInFire)
+            {
+                coroutine = StayInFireDamage(2f);
+                StartCoroutine(coroutine);
+                PlayerHealth playerTarget = collision.transform.gameObject.GetComponent<PlayerHealth>();
+                playerTarget.ApplyDamage(damage);
+                isInFire = false;
+            }
+        
+            else if(collision.gameObject.tag=="Enemy" && isInFire)
+            {
+                coroutine = StayInFireDamage(2f);
+                StartCoroutine(coroutine);
+                EnemyTarget target = collision.transform.gameObject.GetComponent<EnemyTarget>();
+                target.ApplyDamage(damage);
+                isInFire = false;
+           }
     }
+
+    private IEnumerator StayInFireDamage(float waitTime)
+    {
+            yield return new WaitForSeconds(waitTime);
+            isInFire = true;
+            StopCoroutine(coroutine);     
+    }
+
    void OnTriggerExit(Collider collision)
    {
-       if(collision.gameObject.tag=="Player")
-        {   
-            damage = 10f;   
-            Debug.Log("Player Exits fire");
-        }
-        else if(collision.gameObject.tag=="Enemy")
-        {
-            EnemyTarget target = collision.transform.gameObject.GetComponent<EnemyTarget>();
-            target.ApplyDamage(damage);
-        }
    }
+  
 }
