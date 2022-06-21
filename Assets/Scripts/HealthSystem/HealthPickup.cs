9d34c429ef420bad8658c93aeadd7bd9d9f7f37e
Slider <cph-yk31@cphbusiness.dk>
2022-06-21 11:39:21 +0200

    fix nullpointer for health collider

--- Assets/Scripts/HealthSystem/HealthPickup.cs
+++ Assets/Scripts/HealthSystem/HealthPickup.cs
@@ -7,10 +7,12 @@ public class HealthPickup : MonoBehaviour {
     public float amount = 50;
 
     private void OnTriggerEnter(Collider other) {
-        PlayerHealth health = other.GetComponent<PlayerHealth>();
-        if (health.health < 100f) {
+        if (other.gameObject.tag == "Player"){
+          PlayerHealth health = other.GetComponent<PlayerHealth>();
+            if (health.health < 100f) {
             health.RestoreHealth(amount);
             Destroy(gameObject);
-        }
+            }  
+        } 
     }
 }
